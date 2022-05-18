2f19e14ebc8ef157041e13e9501ece99380cea9d
Svante Boberg <svante.boberg@gmail.com>
2022-05-18 17:15:34 +0200

    Don't cut css props with multiple values (i.e. margin: 5px 10px)

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ const transformProps = props => {
 
               return t.objectProperty(
                 t.stringLiteral(key.replace(/-./g, (x) => x[1].toUpperCase())),
-                val.endsWith('px') ?
+                !val.includes(' ') && val.endsWith('px') ?
                   t.numericLiteral(parseFloat(val.split('px')[0]))
                   : t.stringLiteral(val),
               )
